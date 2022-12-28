6cc3fcaa320b033fbb5d6fb34640d2390fc01c8a
seforaqm <106718859+seforaqm@users.noreply.github.com>
2022-12-27 22:16:48 -0500

    Passport JWT

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const partnerRouter = express.Router();
+const authenticate = require('../authenticate');
 
 const Partner = require('../models/partner');
 
@@ -13,7 +14,7 @@ partnerRouter.route('/')
             })
             .catch(err => next(err))
     })
-    .post((req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
         Partner.create(req.body)
             .then(partner => {
                 res.statusCode = 201;
@@ -22,11 +23,11 @@ partnerRouter.route('/')
             })
             .catch(err => next(err))
     })
-    .put((req, res) => {
+    .put(authenticate.verifyUser, (req, res) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /partners');
     })
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser, (req, res, next) => {
         Partner.deleteMany()
             .then(partners => {
                 res.statusCode = 200;
@@ -42,18 +43,18 @@ partnerRouter.route('/:partnerId')
             .then(partner => res.status(200).json(partner))
             .catch(err => next(err))
     })
-    .post((req, res) => {
+    .post(authenticate.verifyUser, (req, res) => {
         res.statusCode = 403;
         res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
     })
-    .put((req, res, next) => {
+    .put(authenticate.verifyUser, (req, res, next) => {
         Partner.findByIdAndUpdate(req.params.partnerId, {
             $set: req.body
         }, { new: true })
             .then(partner => res.status(200).json(partner))
             .catch(err => next(err))
     })
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser, (req, res, next) => {
         Partner.findByIdAndDelete(req.params.partnerId)
             .then(partner => res.status(200).json(partner))
             .catch(err => next(err))
